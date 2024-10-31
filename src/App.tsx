537ee74cbc62dfaa28d5e27718d2804b3b1e1c19
Pavel_Kazlou <pnkazlou@ibagroup.eu>
2024-10-31 17:52:51 +0300

    add useTelegram

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import "./App.scss";
 import { Route, Routes } from "react-router-dom";
 import Content from "./components/Content/Content";
+import { useTelegram } from "./hooks/useTelegram";
+import { useEffect } from "react";
 
 function App() {
+  const { tg } = useTelegram();
+
+  useEffect(() => {
+    tg.ready();
+    tg.expand();
+  }, []);
+
   return (
     <Routes>
       <Route path="/:id" element={<Content />} />
+      <Route path="*" element={<h1>NOT FOUND</h1>} />
     </Routes>
   );
 }
